8770a157a6561f6df459a1fcfa0c2989f47c5ca8
jiaojian <jiaojian0213@gmail.com>
2018-10-18 14:53:55 +0800

    详情添加保存按钮

--- app/src/main/java/com/example/jiao/demo/ui/CitySiteActivity.java
+++ app/src/main/java/com/example/jiao/demo/ui/CitySiteActivity.java
@@ -14,7 +14,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.EditText;
@@ -121,8 +124,15 @@ public class CitySiteActivity extends BaseActivity implements ViewTreeObserver.O
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                finish();
+            }
+        });
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
                 saveInfo();
                 finish();
+                return true;
             }
         });
         Intent intent = getIntent();
@@ -256,6 +266,12 @@ public class CitySiteActivity extends BaseActivity implements ViewTreeObserver.O
                 .getCitySiteModelDao()
                 .update(citySiteModel);
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("保存").setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        return super.onCreateOptionsMenu(menu);
     }
 
     public String getTextStr(EditText view) {
